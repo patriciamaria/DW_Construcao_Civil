fa993d2fdbfc0ceac3b0e625c886e1ffffe83485
Patricia <pati270297@gmail.com>
2019-03-03 10:33:03 -0300

    Exclusao

--- run.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-from ConsApp import app  
-
-if __name__ == '__main__':
-    app.run(debug=True) 
--- setup.py
+++
@@ -1,5 +0,0 @@
-from ConsApp import db
-
-db.drop_all() 
-db.create_all()
-print('done.')
