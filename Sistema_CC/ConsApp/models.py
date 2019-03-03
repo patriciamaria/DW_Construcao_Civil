5db8398b18b585fb74e320932329ef8857729102
Patricia <pati270297@gmail.com>
2019-03-03 10:20:55 -0300

    Organização

---
+++ Sistema_CC/ConsApp/models.py
@@ -0,0 +1,116 @@
+﻿from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from ConsApp import db
+
+class Usuario(db.Model):
+    """Entidade Usuário"""
+    id = db.Column(
+        db.Integer,
+        primary_key = True)
+
+    nome = db.Column(db.String(100))
+    telefone = db.Column(db.String(20))
+    email = db.Column(db.String(50))
+    senha = db.Column(db.String(100))
+    tipo = db.Column(db.String(1))
+
+    id_endereco = db.Column(
+            db.Integer,
+            db.ForeignKey('endereco.id'))
+
+    endereco = db.relationship(
+            'Endereco',
+            backref='usuario',
+            uselist=False)
+    
+    reservas = db.relationship(
+            'Reserva',
+            backref='usuario',
+            lazy='dynamic')
+
+    #Contrutor recebe os atributos nome, telefone e email.
+    def __init__(self, nome, telefone, email, senha, tipo):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.senha = senha
+        self.tipo = tipo
+
+
+class Endereco(db.Model):
+    """Entidade Endereço Do Usuário"""
+    id = db.Column(
+        db.Integer,
+        primary_key=True)
+
+    cep = db.Column(db.String(10))
+    cidade = db.Column(db.String(30))
+    rua = db.Column(db.String(100))
+
+    def __init__(self, cep, cidade, rua):
+        self.cep = cep
+        self.cidade = cidade
+        self.rua = rua
+
+
+produto_reserva = db.Table('produto_reserva',
+    db.Column('id_produto', db.Integer, db.ForeignKey('produto.id')),
+    db.Column('id_reserva', db.Integer, db.ForeignKey('reserva.id'))
+)
+
+class Reserva(db.Model):
+    """Entidade Reserva"""
+    id = db.Column(db.Integer, primary_key=True)    
+    des_prod = db.Column(db.String(100))    
+    id_usuario = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    produtos = db.relationship(
+        'Produto',
+        secondary=produto_reserva,
+        backref=db.backref(
+        'reserva',
+        lazy='dynamic'))
+    
+    def __init__(self, des_prod):
+        self.des_prod = des_prod
+    def __repr__(self):
+        return '{} ({})'.format(self.des_prod, self.id_usuario)
+
+class Produto(db.Model):
+    """Entidade Produto"""
+    id = db.Column(
+        db.Integer,
+        primary_key=True)
+
+    nomep = db.Column(db.String(100))
+
+    reservas = db.relationship(
+        'Reserva',
+        secondary=produto_reserva,
+        backref=db.backref(
+        'produto',
+        lazy='dynamic'))
+
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'))
+
+    def __init__(self, nomep):
+        self.nomep = nomep
+    def __repr__(self):
+        return '{}'.format(self.nomep)
+
+class Categoria(db.Model):
+    """Entidade Categoria"""
+    id = db.Column(
+        db.Integer,
+        primary_key=True)
+
+    tipo = db.Column(db.String(100))
+    descricao = db.Column(db.String(200))
+
+    produtos = db.relationship('Produto', backref='categoria',
+                                lazy='dynamic')
+    def __init__(self, tipo, descricao):
+        self.tipo = tipo
+        self.descricao = descricao        
+    def __repr__(self):
+        return '{} ({})'.format(self.tipo, self.descricao)
+        
---
+++ Sistema_CC/management.py
@@ -0,0 +1,241 @@
+from ConsApp import db, models
+from ConsApp.models import *
+
+def vincular_user_adress(user_id, adress_id):
+	user = Usuario.query.get(user_id)
+	adress = Endereco.query.get(adress_id)
+	if user and adress is not None:
+		user.endereco = adress
+		db.session.commit()	
+		return user
+	else:
+		return None
+
+def vincular_user_reserva(user_id, reserva_id):
+	user = Usuario.query.get(user_id)
+	reserva = Reserva.query.get(reserva_id)
+	if user and Reserva is not None:
+		user.reserva = reserva
+		reserva.id_usuario = user_id
+		db.session.commit()	
+		return user
+	else:
+		return None
+	
+def vincular_reserva_produto(reserva_id, produto_id):
+	reserva = Produto.query.get(reserva_id)
+	produto = Produto.query.get(produto_id)
+	if reserva and produto is not None:
+		reserva.produtos.append(produto) 
+		livro.reservas.append(reserva)
+		db.session.commit()
+		return reserva
+	else:
+		return None
+
+def vincular_produto_categoria(produto_id, categoria_id):
+	produto = Produto.query.get(produto_id)
+	categoria = Categoria.query.get(categoria_id)
+	if produto and categoria is not None:
+		produto.categoria_id = categoria.id
+		categoria.produtos.append(produto)
+		db.session.commit()
+		return produto
+	else:
+		return None
+
+#CRUD ENTIDADE USUÁRIO
+def obter_usuarios():
+	users = Usuario.query.all()
+	
+	lista_usuarios = list()
+
+	if users is not None:
+		for item in users:
+			if item.endereco is not None:
+				lista_usuarios.append({'Id':item.id, 'Nome':item.nome, 'Telefone':item.telefone, 'Email':item.email, 'Senha':item.senha, 'Id_Endereço':item.endereco.id, 'Rua':item.endereco.rua, 'Cep':item.endereco.cep, 'Cidade':item.endereco.cidade, 'Tipo':item.tipo,})
+			else:
+				lista_usuarios.append({'Id':item.id, 'Nome':item.nome, 'Telefone':item.telefone, 'Email':item.email, 'Senha':item.senha, 'Tipo':item.tipo,})	
+
+		return lista_usuarios
+	else:
+		return None
+
+def obter_usuario(usuario_id):
+	user = Usuario.query.get(usuario_id)
+	if user is not None:
+		return user
+	else:
+		return None
+
+def validar_login(usuario_email, usuario_senha):
+	user = Usuario.query.filter(Usuario.email==usuario_email, Usuario.senha==usuario_senha).first()
+	print(user)
+	if user is not None:
+		return ({'id':user.id, 'Email':user.email, 'Senha':user.senha, 'Nome':user.nome, 'Telefone':user.telefone, 'Tipo':user.tipo})
+	else:
+		return None
+
+
+def inserir_usuario(nome, telefone, email, senha, tipo):
+	user = Usuario(nome, telefone, email, senha, tipo)
+	db.session.add(user)
+	db.session.commit()
+	return user
+
+def atualizar_usuario(id, nome, telefone, email, senha, tipo):
+	user = Usuario.query.get(id)
+	if user is not None:
+		user.nome = nome
+		user.telefone = telefone
+		user.email = email
+		user.senha = senha
+		user.tipo = tipo
+		db.session.commit()
+	else:
+		return None
+
+def deletar_usuario(id):
+	user = Usuario.query.get(id)
+
+	if user is not None:
+		db.session.delete(user)
+		db.session.commit()
+		return user
+	else:
+		return None
+
+#CRUD ENTIDADE ENDEREÇO
+def inserir_endereco(cep, cidade, rua):
+	adress = Endereco(cep, cidade, rua)
+	db.session.add(adress)
+	db.session.commit()
+	return adress
+
+def atualizar_endereco(id, cep, cidade, rua):
+	adress = Endereco.query.get(id)
+	
+	if adress is not None:
+		adress.cep = cep
+		adress.cidade = cidade
+		adress.rua = rua
+		db.session.commit()
+		return adress
+	else:
+		return None
+
+def deletar_endereco(id):
+	adress = Endereco.query.get(id)
+
+	if adress is not None:
+		db.session.delete(adress)
+		db.session.commit()
+		return True
+	else:
+		return None
+
+def obter_endereco(id):
+	adress = Endereco.query.get(id)
+
+	if adress is not None:
+		return adress
+	else:
+		return None
+
+#CRUD ENTIDADE CATEGORIA
+def inserir_categoria(tipo, descricao):
+	categoria = Categoria(tipo, descricao)
+	db.session.add(categoria)
+	db.session.commit()
+	return categoria
+
+def atualizar_categoria(id, tipo, descricao):
+	categoria = Categoria.query.get(id)
+	if categoria is not None:
+		categoria.tipo = tipo
+		categoria.descricao = descricao
+		db.session.commit()
+		return categoria
+	else:
+		return None
+
+def deletar_categoria(id):
+	categoria = Categoria.query.get(id)
+	if categoria is not None:
+		db.session.delete(categoria)
+		db.session.commit()
+		return categoria
+	else:
+		return None
+
+def obter_categoria(id):
+	categoria = Categoria.query.get(id)
+	if categoria is not None:
+		return categoria
+	else:
+		return None
+
+
+#CRUD ENTIDADE PRODUTO
+def inserir_produto(nomep):
+	produto = Produto(nomep)
+	db.session.add(produto)
+	db.session.commit()
+	return produto
+
+def atualizar_produto(id, nomep):
+	produto = Produto.query.get(id)
+	if livro is not None:
+		produto.nomep = nomep	
+		db.session.commit()
+		return produto
+	else:
+		return None
+
+def deletar_produto(id):
+	produto = Produto.query.get(id)
+	if produto is not None:
+		db.session.delete(produto)
+		db.session.commit()
+		return produto
+	else:
+		return None
+
+def obter_produto(id):
+	produto = Produto.query.get(id)
+	if produto is not None:
+		return produto
+	else:
+		return None
+
+#CRUD ENTIDADE RESERVA
+def inserir_reserva(des_prod):
+	reserva = reserva(des_prod)
+	db.session.add(reserva)
+	db.session.commit()
+	return reserva
+
+def atualizar_reserva(id, des_prod):
+	reserva = Reserva.query.get(id)
+	if reserva is not None:
+		reserva.des_prod = des_prod
+		db.session.commit()
+		return reserva
+	else:
+		return None
+
+def deletar_reserva(id):
+	reserva = Reserva.query.get(id)
+	if reserva is not None:
+		db.session.delete(reserva)
+		db.session.commit()
+		return reserva
+	else:
+		return None
+
+def obter_reserva(id):
+	reserva = Reserva.query.get(id)
+	if reserva is not None:
+		return reserva
+	else:
+		return None
